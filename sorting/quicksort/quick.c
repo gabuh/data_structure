4131f5a68917dde9067d9840b7bbba488b4fa472
Gabriel <nogueira.santos@aluno.ifsp.edu.br>
2022-09-15 23:30:48 -0300

    quicksort beg

---
+++ sorting/quicksort/quick.c
@@ -0,0 +1,33 @@
+/*
+Understanding the Quicksort algorithm
+Date 9-15-22
+@gabuh
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10
+
+int quicksort(){
+
+
+return smethin;
+}
+
+
+
+int main(){
+
+int vector[SIZE]={};
+
+for(int i=0;i<SIZE;i++){
+vector[i]=rand()%10;
+}
+
+for(int i=0;i<SIZE;i++){
+printf("%d ",vector[i]);
+}
+printf("\n");
+return 0;
+}
